04cfa377558a43d3afdda470dcadbd442f925b21
Frantisek Troster <frantisek.troster@gmail.com>
2016-01-12 16:44:26 +0100

    Moved DefaultController to Service to make it more flexible

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,10 +2,9 @@
 
 namespace Coral\SiteBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
@@ -14,47 +13,12 @@ class DefaultController extends Controller
      */
     public function menuAction($max_level, $uri = null)
     {
-        $sitemap = $this->get('coral.sitemap');
-
         return $this->render(
             'CoralSiteBundle:Default:menu.html.twig',
             array(
-                'parent'      => $sitemap->getRoot(),
+                'parent'      => $this->get('coral.sitemap')->getRoot(),
                 'max_level'   => $max_level - 1,
                 'current_url' => (null === $uri) ? $this->getRequest()->getPathInfo() : $uri
-            )
-        );
-    }
-
-    public function pageAction()
-    {
-        $page = $this->get('coral.page');
-
-        //Permission property, validate authentication
-        if(
-            $page->getNode()->hasProperty('permission')
-            &&
-            (false === $this->get('security.authorization_checker')->isGranted($page->getNode()->getProperty('permission')))
-        )
-        {
-            throw $this->createAccessDeniedException('Unable to access this page!');
-        }
-        //Redirection property
-        if($page->getNode()->hasProperty('redirect'))
-        {
-            return $this->redirect($page->getNode()->getProperty('redirect'), 301);
-        }
-        //Placeholder property = Node is for structure only without content
-        if($page->getNode()->hasProperty('placeholder'))
-        {
-            throw $this->createNotFoundException('Page not found exception. Node is a placeholder.');
-        }
-
-        return $this->render(
-            $page->getNode()->getProperty('template', 'CoralSiteBundle:Default:page.html.twig'),
-            array(
-                'page'     => $page,
-                'renderer' => $this->get('coral.renderer')
             )
         );
     }
---
+++ Controller/PageController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Coral\SiteBundle\Controller;
+
+use Coral\SiteBundle\Service\Page;
+use Coral\SiteBundle\Service\Renderer;
+use Coral\SiteBundle\Service\Sitemap;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Templating\EngineInterface;
+
+class PageController
+{
+    /**
+     * @var \Symfony\Component\Templating\EngineInterface
+     */
+    private $templating;
+    /**
+     * @var \Coral\SiteBundle\Service\Page
+     */
+    private $page;
+    /**
+     * @var \Coral\SiteBundle\Service\Renderer
+     */
+    private $renderer;
+    /**
+     * @var \Coral\SiteBundle\Service\Sitemap
+     */
+    private $sitemap;
+    /**
+     * @var \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface
+     */
+    private $authChecker;
+
+    public function __construct(EngineInterface $templating, Page $page, Sitemap $sitemap, Renderer $renderer, AuthorizationCheckerInterface $authChecker)
+    {
+        $this->templating  = $templating;
+        $this->page        = $page;
+        $this->sitemap     = $sitemap;
+        $this->renderer    = $renderer;
+        $this->authChecker = $authChecker;
+    }
+
+    public function pageAction()
+    {
+        //Permission property, validate authentication
+        if(
+            $this->page->getNode()->hasProperty('permission')
+            &&
+            (false === $this->authChecker->isGranted($this->page->getNode()->getProperty('permission')))
+        )
+        {
+            throw new AccessDeniedException('Unable to access this page!');
+        }
+        //Redirection property
+        if($this->page->getNode()->hasProperty('redirect'))
+        {
+            return new RedirectResponse($this->page->getNode()->getProperty('redirect'), 301);
+        }
+        //Placeholder property = Node is for structure only without content
+        if($this->page->getNode()->hasProperty('placeholder'))
+        {
+            throw new NotFoundHttpException('Page not found exception. Node is a placeholder.');
+        }
+
+        return $this->templating->renderResponse(
+            $this->page->getNode()->getProperty('template', 'CoralSiteBundle:Default:page.html.twig'),
+            array(
+                'page'     => $this->page,
+                'renderer' => $this->renderer
+            )
+        );
+    }
+}
