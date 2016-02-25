a9a9a98b6a01bdce81b6c24253d95b6e3e10f4b4
Frantisek Troster <frantisek.troster@gmail.com>
2016-02-25 13:30:51 +0100

    Updated to work with SF3 and fixed Controller::getRequest

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,20 +5,21 @@ namespace Coral\SiteBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/menu/{max_level}")
      */
-    public function menuAction($max_level, $uri = null)
+    public function menuAction($max_level, $uri = null, Request $request)
     {
         return $this->render(
             'CoralSiteBundle:Default:menu.html.twig',
             array(
                 'parent'      => $this->get('coral.sitemap')->getRoot(),
                 'max_level'   => $max_level - 1,
-                'current_url' => (null === $uri) ? $this->getRequest()->getPathInfo() : $uri
+                'current_url' => (null === $uri) ? $request->getPathInfo() : $uri
             )
         );
     }
