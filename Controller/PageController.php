6b49999d0a1359d703603f731b7c46e8581ee77e
Frantisek Troster <frantisek.troster@gmail.com>
2019-06-11 18:30:23 +0200

    Removed unnecessary use

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -6,11 +6,8 @@ use Coral\SiteBundle\Service\Page;
 use Coral\SiteBundle\Service\Renderer;
 use Coral\SiteBundle\Service\Sitemap;
 use Twig\Environment;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
